628923ac11841e0cadc5571014a68473d42ca5e7
jaffarabbas <gamaportal8@gmail.com>
2023-07-08 19:18:03 +0500

    Commited File : japenese-city.sql at 08/07/2023 7:18:03 pm

---
+++ japenese-city.sql
@@ -0,0 +1,6 @@
+-- Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select  * from CITY where COUNTRYCODE = 'JPN'
