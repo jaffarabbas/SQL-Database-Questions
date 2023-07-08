cd2bfb131f0da5486dfbd490d2c41314bc58f8e6
jaffarabbas <gamaportal8@gmail.com>
2023-07-08 23:36:35 +0500

    Commited File : select-all.sql at 08/07/2023 11:36:35 pm

---
+++ select-all.sql
@@ -0,0 +1,6 @@
+-- Query all columns (attributes) for every row in the CITY table.
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select * from CITY
