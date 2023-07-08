5c4dea0a855b41e607e79b858bc68c63d198968b
jaffarabbas <gamaportal8@gmail.com>
2023-07-08 23:36:36 +0500

    Commited File : weather-4.sql at 08/07/2023 11:36:35 pm

--- weather-4.sql
+++ weather-4.sql
@@ -1,3 +1,8 @@
 -- Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
 
-select COUNT(CITY) - COUNT(DISTINCT CITY) from STATION
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT COUNT(*) - COUNT(DISTINCT CITY) AS Difference
+FROM STATION;
